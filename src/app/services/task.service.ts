3e04ee1c139606083badafc2e05ad121b383c177
DiegoCadena <diego@graywolf.co>
2022-10-19 18:25:01 -0600

    Pestaña 1 funcionalidad

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private tasks: string[]=[];
+  constructor() {
+    //Tener informacion al inicio
+    this.tasks.push('Tarea 1');
+    this.tasks.push('Tarea 2');
+   }
+   //Añadir metodos para manejar tareas
+   public getTasks(): string[]{
+    return this.tasks;
+   }
+   public addTask(task: string){
+    this.tasks.push(task);
+   }
+   public deleteTask(pos: number){
+    this.tasks.splice(pos,1);
+   }
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-
+import { TaskService } from '../services/task.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  //Crear atributo para tareas
+  public tasks:string[];
+  public task:string;
+  //Conectarse al servicio al entrar al homepage
+  //Tener que importar servicio como parametro para poder manejarlo
+  //Al instanciar el taskservice se corre su constructor
+  constructor(private taskService:TaskService) {
+    this.tasks=this.taskService.getTasks();
+    this.task="algo";
+  }
 
-  constructor() {}
+  //Crear método para añadir tareas
+  //Como task es publico y enlaado al input, no se requiere de parámetro
+  public addTask(){
+    //Hacer uso de servicio para añadir la tarea
+    /*Declarar un atributo y recibir con parametro*/
+    this.taskService.addTask(this.task);
+    //Actualizar arreglo local
+    //Usar servicio
+    this.tasks=this.taskService.getTasks();
+    //Comprobar
+    console.log(this.tasks);
+    //Borrar input
+    this.task=""
+    
+    
+  }/*Tarea: UX, que con un enter guarde la tarea
+      Tarea: Que ponga el focus en el input tras agregar tarea 
+      
+      Borrar task*/
+  public removeTask(pos:number){
+    this.taskService.deleteTask(pos);
+    //Actualizar arreglo
+    this.tasks=this.taskService.getTasks();
+  }
+  /*Controlador completo, para visualizar lista se maneja en html */
 
 }
